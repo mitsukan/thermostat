00456edda069f00d6cbe14d273f733784bdfe701
Jihin Ip <jim_ip20@hotmail.com>
2018-04-23 20:07:32 +0100

    Refactoring code to include private functions

--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -1,54 +1,57 @@
-var Thermostat = function() {
+// variable hoisting
+// prototype (y?)
+// debugger (y)
 
-  this.temperature = 20
-  this.powerSave = true
+function Thermostat() {
+  this._temperature = 20
+  this._powerSave = true
+};
 
-  Thermostat.prototype.check = function() {
-    return this.temperature;
-  };
+Thermostat.prototype.check = function() {
+  return this._temperature;
+};
 
-  Thermostat.prototype.up = function() {
-    if(this.powerSave == true && this.temperature < 25) {
-      this.temperature += 1;
-    }
-    else if(this.powerSave == false && this.temperature < 32) {
-      this.temperature +=1;
-    }
-    return this.temperature
-  };
+Thermostat.prototype.up = function() {
+  if(this._powerSave == true && this._temperature < 25) {
+    this._temperature += 1;
+  }
+  else if(this._powerSave == false && this._temperature < 32) {
+    this._temperature +=1;
+  }
+  return this._temperature
+};
 
-  Thermostat.prototype.down = function() {
-    if(this.temperature > 10) {
-      return this.temperature -= 1;
-    }
-  };
+Thermostat.prototype.down = function() {
+  if(this._temperature > 10) {
+    return this._temperature -= 1;
+  }
+};
 
-  Thermostat.prototype.checkPS = function() {
-    return this.powerSave;
-  };
+Thermostat.prototype.checkPS = function() {
+  return this._powerSave;
+};
 
-  Thermostat.prototype.togglePS = function() {
-    if(this.powerSave == false) {
-      return this.powerSave = true
-    }
-    else if(this.powerSave == true) {
-      return this.powerSave = false
-    }
-  };
+Thermostat.prototype.togglePS = function() {
+  if(this._powerSave == false) {
+    return this._powerSave = true
+  }
+  else if(this._powerSave == true) {
+    return this._powerSave = false
+  }
+};
 
-  Thermostat.prototype.reset = function() {
-    this.temperature = 20
-  };
+Thermostat.prototype.reset = function() {
+  this._temperature = 20
+};
 
-  Thermostat.prototype.usage = function() {
-    if(this.temperature < 18) {
-      return "low-usage"
-    }
-    else if(this.temperature < 25) {
-      return "medium-usage"
-    }
-    else{
-      return "high-usage"
-    }
-  };
+Thermostat.prototype.usage = function() {
+  if(this._temperature < 18) {
+    return "low-usage"
+  }
+  else if(this._temperature < 25) {
+    return "medium-usage"
+  }
+  else{
+    return "high-usage"
+  }
 };
